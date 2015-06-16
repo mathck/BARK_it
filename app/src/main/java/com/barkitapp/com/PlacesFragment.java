f6fefc1bb4c2a22a67c3f63a0e9776dd2540942a
Mateusz Czernecki <mathck@gmail.com>
2015-06-16 10:59:34 +0200

    improved voting experience

---
+++ app/src/main/java/com/barkitapp/com/PlacesFragment.java
@@ -0,0 +1,24 @@
+package com.barkitapp.com;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.barkitapp.android.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlacesFragment extends Fragment {
+
+    public PlacesFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_places, container, false);
+    }
+}
