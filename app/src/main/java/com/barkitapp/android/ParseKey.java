5b2a035cb9730f699fbb092a3eec302d102295d7
Mateusz Czernecki <mathck@gmail.com>
2015-08-10 20:13:12 +0200

    Use Alpha keys if Version name is Alpha

--- app/src/main/java/com/barkitapp/android/ParseKey.java
+++ app/src/main/java/com/barkitapp/android/ParseKey.java
@@ -2,8 +2,6 @@ package com.barkitapp.android;
 
 public final class ParseKey {
 
-    public static final boolean USING_ALPHA = false;
-
     private static final String PARSE_APPLICATION_ID = "bPaekY4XHBk0wDHQlhHwXPSZUTIwGI5m4s8vxcxt";
     private static final String PARSE_APPLICATION_ID_TEST = "dnZ82HoAaXdy5gm6yIXMlpf4d8HIDDUhPdWp5Uac"; // testing
 
@@ -11,10 +9,14 @@ public final class ParseKey {
     private static final String PARSE_CLIENT_KEY_TEST = "x95ee5lAhfDkqIeedssUQw2Ik4JbwGtCyuYof2yM"; // testing
 
     public static String getAppId() {
-        return USING_ALPHA ? PARSE_APPLICATION_ID_TEST : PARSE_APPLICATION_ID;
+        return isAlpha() ? PARSE_APPLICATION_ID_TEST : PARSE_APPLICATION_ID;
     }
 
     public static String getClientKey() {
-        return USING_ALPHA ? PARSE_CLIENT_KEY_TEST : PARSE_CLIENT_KEY;
+        return isAlpha() ? PARSE_CLIENT_KEY_TEST : PARSE_CLIENT_KEY;
+    }
+
+    private static boolean isAlpha() {
+        return BuildConfig.VERSION_NAME.equals("Alpha");
     }
 }
